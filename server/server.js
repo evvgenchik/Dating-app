da78177b3ff8a4fd8c6ff77099409a35cebb7afe
Evgen <yevgenchikk@mail.ru>
2023-05-22 22:51:52 +0300

    feat: add initial server with initial user model

---
+++ server/server.js
@@ -0,0 +1,49 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { MongoClient } from 'mongodb';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import mongoose from 'mongoose';
+dotenv.config();
+
+const usi = 'mongodb+srv://User:user@cluster0.aiauka7.mongodb.net/';
+const app = express();
+const PORT = process.env.PORT || 8000;
+const DBPORT = process.env.DB_URL;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+// app.get('/', (req, res) => {
+//   res.json('Hello to my app');
+// });
+
+// app.post('/signup', (req, res) => {
+//   const client = new MongoClient(uti);
+// });
+
+// app.get('/users', async (req, res) => {
+//   const client = new MongoClient(uti);
+
+//   try {
+//     await client.connect();
+//     const dataBase = client.db();
+//   } finally {
+//     await client.close();
+//   }
+// });
+
+const start = async () => {
+  try {
+    await mongoose.connect(DBPORT, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    app.listen(PORT, () => console.log(`Server started on PORT: ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
