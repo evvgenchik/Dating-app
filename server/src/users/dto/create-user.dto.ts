7492d62ac0dc826c5094ffb64ceb9089b4bd77cb
Evgen <yevgenchikk@mail.ru>
2023-06-27 19:09:04 +0300

    feat: add crate dto with validato pipe

--- server/src/users/dto/create-user.dto.ts
+++ server/src/users/dto/create-user.dto.ts
@@ -1,1 +1,23 @@
-export class CreateUserDto {}
+import { IsEmail, IsString, Length, Min } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  email: string;
+  @IsString({ message: '$property must be a string' })
+  @Min(4)
+  password: string;
+  @IsString({ message: '$property must be a string' })
+  firstName: string;
+  @IsString({ message: '$property must be a string' })
+  birthday: string;
+  @IsString({ message: '$property must be a string' })
+  country: string;
+  @IsString({ message: '$property must be a string' })
+  gender: string;
+  @IsString({ message: '$property must be a string' })
+  looking: string;
+  @IsString({ message: '$property must be a string' })
+  @Length(5, 300)
+  descriptrion: string;
+  photo: string;
+}
