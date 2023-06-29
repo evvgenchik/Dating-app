6aaf77dec9b8c6de8bec9a0a724329edf726395c
Evgen <yevgenchikk@mail.ru>
2023-06-29 11:18:48 +0300

    fix: email confirm by link

--- server/src/emailConfirm/emailConfirm.controller.ts
+++ server/src/emailConfirm/emailConfirm.controller.ts
@@ -1,16 +1,25 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Redirect,
+  Req,
+  Res,
+} from '@nestjs/common';
 import { EmailConfirmService } from './emailConfirm.service';
 import ConfirmEmailDto from './dto/confirmEmail.dto';
+import { Request, Response } from 'express';
 
 @Controller('email-confirm')
 export class EmailConfirmController {
   constructor(private readonly emailConfirmService: EmailConfirmService) {}
 
-  @Post('confirm')
-  async confirm(@Body() confirmationData: ConfirmEmailDto) {
-    const email = await this.emailConfirmService.decodeConfirmationToken(
-      confirmationData.token,
-    );
+  @Get('')
+  async confirm(@Req() req: Request, @Res() res: Response) {
+    const token = req.query.token.toString();
+    const email = await this.emailConfirmService.decodeConfirmationToken(token);
     await this.emailConfirmService.confirmEmail(email);
+    res.redirect('http://localhost:3000/app');
   }
 }
