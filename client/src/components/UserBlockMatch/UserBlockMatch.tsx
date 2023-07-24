f34a8cbe4749ecb652e1cf6370d5c5421484769f
Evgen <yevgenchikk@mail.ru>
2023-07-24 11:07:09 +0300

    fix: delete wrapper

--- client/src/components/UserBlockMatch/UserBlockMatch.tsx
+++ client/src/components/UserBlockMatch/UserBlockMatch.tsx
@@ -10,44 +10,41 @@ const content = {
   Messages: <Messages />,
 };
 
-function UserBlockMatch() {
+function UserBlockMatch({ isShow }) {
   const [block, setBlock] = useState('Matches');
   const { user } = useContext(AuthContext);
 
-  const [isShow, setIsShow] = useState<boolean>(false);
+  // const [isShow, setIsShow] = useState<boolean>(false);
 
-  const showHandler = () => {
-    setIsShow(!isShow);
-  };
+  // const showHandler = () => {
+  //   setIsShow(!isShow);
+  // };
 
   return (
-    <div className={`${styles.wrapper} ${isShow && styles.active}`}>
-      <MessageIcon onClick={showHandler} className={styles.showBtn} />
-      <div className={`${styles.content} ${isShow && styles.active}`}>
-        <div className={styles.header}>
-          <div className={styles.photoContainer}>
-            <img className={styles.photo} src={user.avatar} alt='profile' />
-          </div>
-          <span className={styles.name}>{user.firstName}</span>
+    <div className={`${styles.content} ${isShow && styles.active}`}>
+      <div className={styles.header}>
+        <div className={styles.photoContainer}>
+          <img className={styles.photo} src={user.avatar} alt='profile' />
         </div>
-        <div className={styles.controlPanel}>
-          <button
-            onClick={() => setBlock('Matches')}
-            className={styles.controlBtn}
-            type='button'
-          >
-            Matches
-          </button>
-          <button
-            onClick={() => setBlock('Messages')}
-            className={styles.controlBtn}
-            type='button'
-          >
-            Messages
-          </button>
-        </div>
-        <div className={styles.main}>{content[block]}</div>
+        <span className={styles.name}>{user.firstName}</span>
       </div>
+      <div className={styles.controlPanel}>
+        <button
+          onClick={() => setBlock('Matches')}
+          className={styles.controlBtn}
+          type='button'
+        >
+          Matches
+        </button>
+        <button
+          onClick={() => setBlock('Messages')}
+          className={styles.controlBtn}
+          type='button'
+        >
+          Messages
+        </button>
+      </div>
+      <div className={styles.main}>{content[block]}</div>
     </div>
   );
 }
--- client/src/pages/Match/Match.tsx
+++ client/src/pages/Match/Match.tsx
@@ -1,11 +1,21 @@
 import styles from './Match.module.scss';
+import { RiMessage2Line as MessageIcon } from 'react-icons/ri';
 import UserBlockMatch from '@/components/UserBlockMatch/UserBlockMatch';
+import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 
 function Match() {
+  const [isShow, setIsShow] = useState<boolean>(false);
+
+  const showHandler = () => {
+    setIsShow(!isShow);
+  };
+
   return (
     <div className={styles.content}>
-      <UserBlockMatch />
+      <MessageIcon onClick={showHandler} className={styles.showBtn} />
+
+      <UserBlockMatch isShow={isShow} />
       <div className={styles.mainContent}>
         <Outlet />
       </div>
