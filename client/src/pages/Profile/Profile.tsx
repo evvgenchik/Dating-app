9d04eb57318fd2874112059770ca074a287b9506
Evgen <yevgenchikk@mail.ru>
2023-07-27 17:12:37 +0300

    fix:layout

--- client/src/pages/Profile/Profile.tsx
+++ client/src/pages/Profile/Profile.tsx
@@ -1,16 +1,11 @@
 import { useContext } from 'react';
 import SignUp from '../SignUp/SignUp';
 import AuthContext from '@/context/authProvider';
-import styles from './Profile.module.scss';
 
 function Profile() {
   const { user } = useContext(AuthContext);
 
-  return (
-    <div>
-      <SignUp currentUser={user} />
-    </div>
-  );
+  return <SignUp currentUser={user} />;
 }
 
 export default Profile;
