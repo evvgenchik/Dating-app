ce310699982ce7e023e224c57497e74bb8fee41c
Evgen <yevgenchikk@mail.ru>
2023-08-03 18:01:52 +0300

    fix: debug deploy on client

--- client/src/components/MainDisplayMatch/Chat/ChatDisplay/ChatDisplay.tsx
+++ client/src/components/MainDisplayMatch/Chat/ChatDisplay/ChatDisplay.tsx
@@ -17,6 +17,7 @@ function ChatDisplay() {
   const [conversation, setConversation] = useState<ConversationType>(null);
   const conversationRef = useRef<ConversationType>();
   const anchorRef = useRef<HTMLDivElement>();
+  console.log('convers ' + conversation?.id);
 
   useEffect(() => {
     anchorRef.current?.scrollIntoView({ behavior: 'smooth' });
@@ -37,8 +38,12 @@ function ChatDisplay() {
     };
 
     socket.emit('getConversationForEmails', conversationDto);
+    console.log('emit getConversationForEmails');
 
     function receiveConversation(value: ConversationType) {
+      console.log('recieve convers');
+      console.log(value);
+
       conversationRef.current = value;
       setConversation(value);
     }
